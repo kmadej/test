4276d1facb10e1fee700bb85d73eb410c0b989b2
kmadej <kmadej@soldevelo.com>
2017-11-29 15:23:43 +0100

    TEEEST

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,10 @@ public class HelloWorld {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         System.exit( 0 ); //success
+
+        String aha = new String("aha");
+        aha.contains("ss");
+
+        aha.toLowerCase();
     }
 }
