84736383778395f6733a30267b97137d7ae9e7f4
kmadej <kmadej@soldevelo.com>
2018-01-08 16:49:17 +0100

    fixed

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,12 +2,5 @@ public class HelloWorld {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         System.exit( 0 ); //success
-
-        if (true) System.out.println( "Hello World!" );
-        
-        if (true)
-        {
-
-        }
     }
 }
