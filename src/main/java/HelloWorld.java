edfaea453dde18b18c78e56ca0cce2e01a0c5784
kmadej <kmadej@soldevelo.com>
2018-01-17 14:38:48 +0100

    test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,8 @@ public class HelloWorld {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         System.exit( 0 ); //success
+
+        String aha = new String("aha");
+        aha.toLowerCase();
     }
 }
